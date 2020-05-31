a03df1162447aa933c73b98263ec7cbb8ae73e8a
prince.kumar267@gmail.com <prince.kumar@arvindinternet.com>
2020-05-31 15:08:59 +0530

    Dynamic URL'

--- src/pages/cdp/Cdp.js
+++ src/pages/cdp/Cdp.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 
 export default class Cdp extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            CollegeName:""
+        }
+    }
+    componentDidMount(){
+        let collegeName=decodeURI(this.props.location.search).replace('?','')
+        this.setState({CollegeName:collegeName})
+    }
     render(){
         return(
             <div>
-                <p>college description page working</p>
+                <p>college name {this.state.CollegeName} description page working</p>
             </div>
         )
     }
