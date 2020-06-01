79a402e9e76458aa402265d35b41c475add0c63a
prince.kumar267@gmail.com <prince.kumar@arvindinternet.com>
2020-06-01 16:41:20 +0530

    Filter Colleges

--- src/server/api/filterData.js
+++ src/server/api/filterData.js
@@ -3,72 +3,40 @@ const router = express.Router();
 const connection = require("../db_connection");
 
 router.get("/", (req, res) => {
+  const city = req.query.city;
+  const state = req.query.state;
+  const course = req.query.course;
+  const Department = req.query.department;
+  let query = `select CollegeDetail.Id,CollegeName,Logo,City,State,Fees,MedianSalary,Rating,Website from CollegeDetail, Courses, Address where CourseId=Courses.id and AddressId=Address.id and Department="${Department}" and`;
 
-    const city = req.query.city;
-    const state = req.query.state;
-    const course = req.query.course;
-    const Department = req.query.department
-    let query = "";
-    if (city != '' && state === '' & course === '') {
-        query = `select 
-        CollegeDetail.Id,CollegeName,Logo,City,State,Fees,MedianSalary,Rating,Website  
-        from CollegeDetail, Courses, Address 
-        where CourseId=Courses.id and AddressId=Address.id and Department="${Department}" and city="${city}";`
-
+  if (city != "" && (state === "") & (course === "")) {
+    query = `${query} city="${city}";`;
+  }
+  if (city === "" && (state != "") & (course === "")) {
+    query = `${query} state="${state}";`;
+  }
+  if (city === "" && (state === "") & (course != "")) {
+    query = `${query} CourseId=${course};`;
+  }
+  if (city != "" && (state != "") & (course === "")) {
+    query = `${query} city="${city}" and state="${state}";`;
+  }
+  if (city != "" && (state === "") & (course != "")) {
+    query = `${query} city="${city}" and CourseId=${course};`;
+  }
+  if (city === "" && (state != "") & (course != "")) {
+    query = `${query} state="${state}" and CourseId=${course};`;
+  }
+  if (city != "" && (state != "") & (course != "")) {
+    query = `${query} city="${city}" and state="${state}" and CourseId=${course};`;
+  }
+  connection.query(query, (err, result, fields) => {
+    if (err) {
+      res.status(400).send({ error: err });
+    } else {
+      res.json(result);
     }
-    if (city === '' && state != '' & course === '') {
-        query = `select 
-        CollegeDetail.Id,CollegeName,Logo,City,State,Fees,MedianSalary,Rating,Website  
-        from CollegeDetail, Courses, Address 
-        where CourseId=Courses.id and AddressId=Address.id and Department="${Department}" and state="${state}";`
-
-    }
-    if (city === '' && state === '' & course != '') {
-        query = `select 
-        CollegeDetail.Id,CollegeName,Logo,City,State,Fees,MedianSalary,Rating,Website  
-        from CollegeDetail, Courses, Address 
-        where CourseId=Courses.id and AddressId=Address.id and Department="${Department}" and CourseId=${course};`
-
-    }
-    if (city != '' && state != '' & course === '') {
-        query = `select 
-        CollegeDetail.Id,CollegeName,Logo,City,State,Fees,MedianSalary,Rating,Website  
-        from CollegeDetail, Courses, Address 
-        where CourseId=Courses.id and AddressId=Address.id and Department="${Department}" and city="${city}" and state="${state}";`
-
-    }
-    if (city != '' && state === '' & course != '') {
-        query = `select 
-        CollegeDetail.Id,CollegeName,Logo,City,State,Fees,MedianSalary,Rating,Website  
-        from CollegeDetail, Courses, Address 
-        where CourseId=Courses.id and AddressId=Address.id and Department="${Department}" and city="${city}" and CourseId=${course};`
-
-    }
-    if (city === '' && state != '' & course != '') {
-        query = `select 
-        CollegeDetail.Id,CollegeName,Logo,City,State,Fees,MedianSalary,Rating,Website  
-        from CollegeDetail, Courses, Address 
-        where CourseId=Courses.id and AddressId=Address.id and Department="${Department}" and state="${state}" and CourseId=${course};`
-
-    }
-    if (city != '' && state != '' & course != '') {
-        query = `select 
-        CollegeDetail.Id,CollegeName,Logo,City,State,Fees,MedianSalary,Rating,Website  
-        from CollegeDetail, Courses, Address 
-        where CourseId=Courses.id and AddressId=Address.id and Department="${Department}" and city="${city}" and state="${state}" and CourseId=${course};`
-
-    }
-    connection.query(
-        query,
-        (err, result, fields) => {
-            if (err) {
-                res.status(400).send({ error: err });
-            } else {
-                console.log(">>> get call...");
-                    res.json(result);
-            }
-        }
-    );
+  });
 });
 
 module.exports = router;
