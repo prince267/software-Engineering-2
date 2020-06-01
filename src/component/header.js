689b2050e28ee5a3406543d52a91e2722d59a002
prince.kumar267@gmail.com <prince.kumar@arvindinternet.com>
2020-06-01 16:17:59 +0530

    Header Component

--- src/component/header.js
+++ src/component/header.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import SchoolIcon from '@material-ui/icons/School';
-import './header.css'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import SchoolIcon from "@material-ui/icons/School";
+import "./header.css";
 import { useHistory } from "react-router-dom";
 
-// import history from './history'
 const useHeaderStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    flexDirection: "row"
+    flexDirection: "row",
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
-    marginRight: 80
+    marginRight: 80,
   },
 }));
 
@@ -30,25 +29,56 @@ export default function Header() {
   return (
     <div>
       <div className={headerClasses.root}>
-        <AppBar style={{ background: '#2E3B55' }}
-          position="static">
-          <Toolbar >
-            <IconButton edge="start" className={headerClasses.menuButton} color="inherit" aria-label="menu"
-              onClick={() => history.push('/')}
+        <AppBar style={{ background: "#2E3B55" }} position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={headerClasses.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={() => history.push("/")}
             >
               <SchoolIcon style={{ fontSize: 30 }} />
             </IconButton>
             <Typography variant="h4" className={headerClasses.title}>
               गुरुकुल
-          </Typography>
-            {/* <Link className='linkStyle' to="/listing">ENGINEERING</Link>
-          <Link className='linkStyle' to="/listing">MEDICAL</Link>
-          <Link className='linkStyle' to="/listing">COMMERCE</Link>
-          <Link className='linkStyle' to="/listing">MBA</Link> */}
-            <Button style={{ marginRight: 20 }} onClick={() => history.push('/listing', { data: "Engineering" })} color="inherit">Engineering</Button>
-            <Button style={{ marginRight: 20 }} onClick={() => history.push('/listing', { data: "Medical" })} color="inherit">Medical</Button>
-            <Button style={{ marginRight: 20 }} onClick={() => history.push('/listing', { data: "Commerce" })} color="inherit">Commerce</Button>
-            <Button style={{ marginRight: 20 }} onClick={() => history.push('/listing', { data: "Management" })} color="inherit">Management</Button>
+            </Typography>
+            <Button
+              style={{ marginRight: 20 }}
+              onClick={() =>
+                history.push("/listing?Engineering", { data: "Engineering" })
+              }
+              color="inherit"
+            >
+              Engineering
+            </Button>
+            <Button
+              style={{ marginRight: 20 }}
+              onClick={() =>
+                history.push("/listing?Medical", { data: "Medical" })
+              }
+              color="inherit"
+            >
+              Medical
+            </Button>
+            <Button
+              style={{ marginRight: 20 }}
+              onClick={() =>
+                history.push("/listing?Commerce", { data: "Commerce" })
+              }
+              color="inherit"
+            >
+              Commerce
+            </Button>
+            <Button
+              style={{ marginRight: 20 }}
+              onClick={() =>
+                history.push("/listing?Management", { data: "Management" })
+              }
+              color="inherit"
+            >
+              Management
+            </Button>
           </Toolbar>
         </AppBar>
       </div>
