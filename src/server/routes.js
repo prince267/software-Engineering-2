42cab00ba0d592403ec74b89a24a1ff8ea06ff4e
prince.kumar267@gmail.com <prince.kumar@arvindinternet.com>
2020-06-01 16:43:30 +0530

    server File

--- src/server/routes.js
+++ src/server/routes.js
@@ -4,18 +4,13 @@ const router = express.Router();
 const app = express();
 
 const get_college = require("./api/getCollege");
-const get_address=require("./api/getAddress")
-const filter_data=require("./api/filterData")
-const post_route = require("./api/post");
-const put_route = require("./api/put");
-const delete_route = require("./api/delete");
-const get_course=require('./api/getCourse')
+const get_address = require("./api/getAddress");
+const filter_data = require("./api/filterData");
+const get_course = require("./api/getCourse");
+
 router.use("/college", get_college);
-router.use("/address",get_address);
-router.use("/course",get_course);
-router.use("/filter",filter_data);
-router.use("/user", post_route);
-router.use("/user", put_route);
-router.use("/user", delete_route);
+router.use("/address", get_address);
+router.use("/course", get_course);
+router.use("/filter", filter_data);
 
 module.exports = router;
--- src/server/server.js
+++ src/server/server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const connection = require("./db_connection"); // database connection
 const routes = require("./routes");
-
+var morgan  = require('morgan')
 const app = express();
+app.use(morgan('dev'))
 app.use(express.json());
 app.use("/", routes);
 app.use(cors())
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hi There</h1?");
+  res.send("<h1>Server Is Running</h1?");
 });
 
-app.listen(8080, () => console.log(">>> App is running on port 8080..."));
+app.listen(8080, () => console.log("Server  is Listening on port 8080..."));
