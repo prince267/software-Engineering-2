684ad75a695685bde4c36aea08c2b51983342e42
prince.kumar267@gmail.com <prince.kumar@arvindinternet.com>
2020-06-01 16:18:34 +0530

    Api File

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 const get = async (url) => {
-    let response = await fetch(url, {
-        method: 'GET',
-        header: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        redirect: 'follow'
-    });
-    let data = await response.json()
-    return data;
-}
+  let response = await fetch(url, {
+    method: "GET",
+    header: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    redirect: "follow",
+  });
+  let data = await response.json();
+  return data;
+};
 
-export {get}
+export { get };
