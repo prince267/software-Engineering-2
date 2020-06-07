27307bf2aeb89fd781484c2c74a440f618809e24
prince.kumar267@gmail.com <prince.kumar@arvindinternet.com>
2020-06-07 15:14:01 +0530

    Server side

--- src/ssr/index.js
+++ src/ssr/index.js
@@ -1,8 +1,14 @@
 require("../../config/env");
 require('ignore-styles')
 
+// require('@babel/register')({
+//   ignore: [/(node_modules)/],
+//   presets: ['@babel/preset-env', '@babel/preset-react','react-app'],
+//   plugins:['transform-es2015-modules-commonjs']
+// })
 require('@babel/register')({
-  ignore: [/(node_modules)/],
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-})
+    ignore: [/(node_modules)/],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins :['@babel/plugin-proposal-export-namespace-from']
+  })
 require('./ssr')
--- src/ssr/ssr.js
+++ src/ssr/ssr.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const { readFileSync  } = require('fs');
+const fs = require('fs');
 const paths = require("../../config/paths");
 
-const filePath = `${paths.appBuild}/index.html`;
-const htmlData = readFileSync(filePath,'utf8');
-console.log(htmlData)
+import React from 'react';
+import { renderToString } from "react-dom/server";
+import App from "../App"
+// const filePath = `${paths.appBuild}/index.html`;
+// const htmlData = readFileSync(filePath,'utf8');
+
+// console.log(htmlData)
 // viewed at http://localhost:8080
 app.use('/', express.static('/home/prince/Documents/SE2/software-engineering-2/build'))
-// app.get('/', function(req, res) {
-//     res.sendFile("/home/prince/Documents/SE2/software-engineering-2/build/index.html");
-// });
 
-app.listen(8080, () => {
+app.get(/^[^.]+$|\.(?!(js|ico|map|css|json)$)([^.]+$)/, (req, res) => {
+    const app = renderToString(<App />);
+  console.log(app)
+    const indexFile = path.resolve( `${paths.appBuild}/index.html`);
+    fs.readFile(indexFile, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Something went wrong:', err);
+        return res.status(500).send('Oops, better luck next time!');
+      }
+  
+      return res.send(
+        data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+      );
+    });
+  });
+app.listen(8081, () => {
     console.log(`App launched on`)});
