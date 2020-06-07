a4b3937fb2b4bc7316c85f5c2cae6cc1267d704d
prince.kumar267@gmail.com <prince.kumar@arvindinternet.com>
2020-06-07 13:18:40 +0530

    some

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -363,6 +363,7 @@ module.exports = function(webpackEnv) {
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
               test: /\.(js|mjs|jsx|ts|tsx)$/,
+              exclude: /node_modules/,
               include: paths.appSrc,
               loader: require.resolve('babel-loader'),
               options: {
--- src/ssr/ssr.js
+++ src/ssr/ssr.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-// const paths = require("../../config/paths");
-// // console.log(paths.appBuild)
+const { readFileSync  } = require('fs');
+const paths = require("../../config/paths");
+
+const filePath = `${paths.appBuild}/index.html`;
+const htmlData = readFileSync(filePath,'utf8');
+console.log(htmlData)
 // viewed at http://localhost:8080
 app.use('/', express.static('/home/prince/Documents/SE2/software-engineering-2/build'))
 // app.get('/', function(req, res) {
 //     res.sendFile("/home/prince/Documents/SE2/software-engineering-2/build/index.html");
 // });
 
-app.listen(8080);
+app.listen(8080, () => {
+    console.log(`App launched on`)});
